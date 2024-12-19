5f98ee2915c1f490c2e9c341c1f9f081398b40aa
24HP7482 24HP7482 <24HP7482@admins-iMac.local>
2024-12-19 16:24:54 +0800

    gg

--- server/routes/likeRoute.js
+++ server/routes/likeRoute.js
@@ -1,6 +1,18 @@
 const Router = require("express");
 const like = require("../controllers/likeController");
+// const likeModel = require("../models/likeSchema");
+const postModel = require("../models/postSchema");
 const likeRoute = Router();
 likeRoute.post("/post/like", like);
-// likeRoute.get()
+likeRoute.get("/like/postId", async (req, res) => {
+  const { postId } = req.query;
+  const getLike = await postModel.find(postId).populate({
+    path: "liked",
+    populate: {
+      path: "userId",
+      select: "username profileImg",
+    },
+  });
+  res.send(getLike);
+});
 module.exports = likeRoute;
