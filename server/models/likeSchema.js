316332b99a4d92631c185d335dd54d3d3bd294be
24HP7482 24HP7482 <24HP7482@admins-iMac.local>
2024-12-19 17:59:44 +0800

    third

--- server/models/likeSchema.js
+++ server/models/likeSchema.js
@@ -2,8 +2,8 @@ const { Schema, default: mongoose } = require("mongoose");
 const userModel = require("../models/userSchema");
 const postModel = require("../models/postSchema");
 const likeSchema = new Schema({
-  postId: [{ type: mongoose.Types.ObjectId, ref: "posts", required: true }],
-  userId: [{ type: mongoose.Types.ObjectId, ref: "users", required: true }],
+  postId: { type: mongoose.Types.ObjectId, ref: "posts", required: true },
+  userId: { type: mongoose.Types.ObjectId, ref: "users", required: true },
 });
 
 const likeModel = mongoose.model("likes", likeSchema);
--- server/routes/likeRoute.js
+++ server/routes/likeRoute.js
@@ -2,6 +2,7 @@ const Router = require("express");
 const like = require("../controllers/likeController");
 // const likeModel = require("../models/likeSchema");
 const postModel = require("../models/postSchema");
+const likeModel = require("../models/likeSchema");
 const likeRoute = Router();
 likeRoute.post("/post/like", like);
 likeRoute.get("/like/postId", async (req, res) => {
@@ -14,5 +15,15 @@ likeRoute.get("/like/postId", async (req, res) => {
     },
   });
   res.send(getLike);
+});
+likeRoute.get("/like/likedUser/:postId", async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const like = await likeModel.find({ postId: postId }).populate("userId");
+    res.status(200).json(like);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 module.exports = likeRoute;
