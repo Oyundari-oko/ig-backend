0b109d3857dfd5084b9ff2fb06aa894d094369e7
24HP7482 24HP7482 <24HP7482@admins-iMac.local>
2025-01-07 18:26:31 +0800

    gg

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -19,6 +19,18 @@ userRoute.get("/signUpUser", async (req, res) => {
 });
 
 // userRoute.post("/login", logIn);
+userRoute.get("/user/post", async (req, res) => {
+  try {
+    const response = await userModel
+      .find()
+      .populate("post", "postImg caption comments");
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 userRoute.get("/user/post/:userId", async (req, res) => {
   const { userId } = req.params;
   try {
